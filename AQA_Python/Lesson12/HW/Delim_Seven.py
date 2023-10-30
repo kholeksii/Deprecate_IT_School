1fea679e2334047d300e5fc7c7d0343e2f94f1cf
kholeksii <oleksiy.kh@gmail.com>
2023-10-31 00:05:14 +0200

    HW for Lesson 12!

---
+++ AQA_Python/Lesson12/HW/Delim_Seven.py
@@ -0,0 +1,2 @@
+result = [number for number in range(0, 1000) if number % 7 == 0]
+print(result)
---
+++ AQA_Python/Lesson12/HW/Under_Decorat.py
@@ -0,0 +1,25 @@
+def func_name(func):
+    def wrapper(*args):
+        print(f'Function title = {func.__name__}')
+
+    return wrapper
+
+@func_name
+def my_max(*args):
+    max_value = args[0]
+    for value in args:
+        if value > max_value:
+            max_value = value
+    return max_value
+
+@func_name
+def my_min(*args):
+    min_value = args[0]
+    for value in args:
+        if value < min_value:
+            min_value = value
+    return min_value
+
+
+my_min(1, 6, 2)
+my_max(1, 6, 2)
