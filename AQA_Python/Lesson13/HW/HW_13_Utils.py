4f49c2023916419947a292159b6caae3638647af
kholeksii <oleksiy.kh@gmail.com>
2023-11-02 21:50:59 +0200

    HW for Lesson 13! Ver 2.0.

--- AQA_Python/Lesson13/HW/HW_13_Utils.py
+++ AQA_Python/Lesson13/HW/HW_13_Utils.py
@@ -1,42 +1,31 @@
 import re
-from examples import text_to_search, urls
 
-def search_in_example(regExp_example, text_message):
+
+def search_by_pattern(regExp_example, text_message, text: str) -> list:
     print(f'{text_message}')
     pattern = re.compile(regExp_example)
-    matchers = pattern.findall(text_to_search)
-    for el in matchers:
-        print(el)
+    return pattern.findall(text)
 
 
-def search_in_example_group(regExp_example, text_message):
+def search_by_pattern_using_group(regExp_example, text_message, text: str) -> list:
     print(f'{text_message}')
-    result = []
     pattern = re.compile(regExp_example)
-    matchers = pattern.finditer(text_to_search)
-    for el in matchers:
-        result.append(el.group())
-        print(el.group())
+    matchers = pattern.finditer(text)
+    return [el.group() for el in matchers]
 
 
-def search_urls_example_by_itter(regExp_example, text_message):
+def search_urls_example(regExp_example, text_message, text: str) -> str:
     print(f'{text_message}')
-    result = []
+
     pattern = re.compile(regExp_example)
-    new_urls = urls.replace('https?://', '')
-    print(new_urls)
-    matchers = pattern.finditer(new_urls)
-    for el in matchers:
-        result.append(el.group())
-        print(el.group())
+    new_urls = pattern.sub(r'\2\3', text)
+    return new_urls
 
 
-def search_in_data(regExp_example, text_message):
+def search_in_file(regExp_example, text_message, file_name) -> list:
     print(f'{text_message}')
     pattern = re.compile(regExp_example)
 
-    with open ('data.txt', 'r') as file:
+    with open(file_name, 'r') as file:
         content = file.read()
-        matchers = pattern.findall(content)
-        for el in matchers:
-            print(el)
+        return pattern.findall(content)
